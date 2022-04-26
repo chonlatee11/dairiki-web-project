a50f610da4fa20a2664635a328d59ba2ddd4a2c1
chonlatee11 <46771457+chonlatee11@users.noreply.github.com>
2022-04-26 16:45:48 +0700

    contact page
    เพิ่มหน้า contact อัพเดท sqlite

--- project/home/models.py
+++ project/home/models.py
@@ -1,3 +1,14 @@
+import email
+from turtle import title
+from unicodedata import name
 from django.db import models
-
 # Create your models here.
+
+class ContactMessage(models.Model) :
+    name = models.CharField(max_length=50)
+    subject = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    detail = models.TextField(blank=False,null=False)
+    
+    def __str__(self):
+        return self.subject
